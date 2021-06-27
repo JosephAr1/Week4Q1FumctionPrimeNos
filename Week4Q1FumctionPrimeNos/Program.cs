262588009ad0275e408988d7e7bc73af823a4fdc
JosephAr1 <jonielhet@yahoo.com>
2021-06-27 23:19:05 +1200

    Add project files.

---
+++ Week4Q1FumctionPrimeNos/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Week4Q1FumctionPrimeNos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1; 
+            int i, count = 0;
+
+            Console.Write("\nPlease Enter a number: "  );
+            num1 = Convert.ToInt32(Console.ReadLine());
+            
+            PrimeNumber(num1);
+        }
+        static void PrimeNumber(int num1)
+        {
+            int count = 0;
+
+            for (int i = 2; i <= num1 / 2; i++)
+            {
+
+                if (num1 % i == 0)
+                {
+                    count = 1;
+                }
+            }
+            if (num1 == 1)
+            {
+                Console.WriteLine(num1.ToString() + "is not a Prime Number");
+            }
+            else
+            {
+                if (count == 0)
+                    Console.WriteLine(num1.ToString() + " is a prime number.");
+                else
+                    Console.WriteLine(num1.ToString() + " is not a prime number.");
+            }
+        }
+    }  
+}
